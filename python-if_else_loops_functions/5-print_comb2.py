4e35e611fe48477a0677016ee21daa94bd6dcd4d
ylabate <12196@holbertonstudents.com>
2026-01-12 14:49:42 +0000

    fix: Correct print formatting in 5-print_comb2.py for proper output

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    print("{:02}".format(i), end='')
     if i < 99:
-        print(", ", end='')
+        print("{:02}, ".format(i), end='')
     else:
-        print("")
+        print("{:02}".format(i))
