739754b10e01039f9506dd058d9f40f796756ad3
ylabate <12196@holbertonstudents.com>
2026-01-13 13:53:34 +0000

    fix: correct pluralization of argument display in 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     lengh = len(sys.argv) - 1
     i = 1
     print("{} argument{}".format(lengh, "s" if lengh > 1 else ""), end='')
-    print("{}".format(":" if lengh > 0 else "."))
+    print("{}{}".format("s" if lengh == 0 else "", ":" if lengh > 0 else "."))
     for arg in sys.argv[1:]:
         print("{}: {}".format(i, arg))
         i += 1
