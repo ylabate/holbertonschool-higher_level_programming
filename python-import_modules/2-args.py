a6a9c0e203b7dfdd5143d5ab982d95a505fa07a6
ylabate <12196@holbertonstudents.com>
2026-01-13 13:48:45 +0000

    fix: correct argument count display in 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    lengh = len(sys.argv) - 1
     i = 1
-    print("{} arguments:".format(len(sys.argv) - 1))
+    print("{} argument{}".format(lengh, "s" if lengh > 1 else ""), end='')
+    print("{}".format(":" if lengh > 0 else "."))
     for arg in sys.argv[1:]:
         print("{}: {}".format(i, arg))
         i += 1
