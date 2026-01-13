6dcebb705b0b5dd682be6291ade0ea4c1fa17986
root <root@ip-10-42-156-231.ec2.internal>
2026-01-13 15:08:36 +0000

    fix: correct logique using importlibb to open hidden file

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+from importlib import util
+import sys
 if __name__ == "__main__":
-    name = dir("/mnt/hidden_4.pyc")
-    for i in name:
+    module = ""
+    spec = util.spec_from_file_location(module, "/tmp/hidden_4.pyc")
+    module = util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    for i in dir(module):
         if i[0] != '_':
             print("{}".format(i))
         
