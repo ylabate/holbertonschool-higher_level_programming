6dd2fa8aa604dd5c856189baa784234b4c6f6f91
ylabate <12196@holbertonstudents.com>
2026-01-19 09:29:24 +0000

    fix: correct loop index in print_reversed_list_integer function to print list elements

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for i in range(len(my_list), 0, -1):
-        print("{:d}".format(i))
+    for i in range(len(my_list) - 1, -1, -1):
+        print("{:d}".format(my_list[i]))
+
+print_reversed_list_integer([1, 2, 'E', 4])
