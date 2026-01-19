87a116dec236f11532d1deead2e16afce94bc8aa
ylabate <12196@holbertonstudents.com>
2026-01-19 13:08:01 +0000

    feat: add max_integer function to find the maximum value in a list

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    biggest = float('-inf')
+    for number in my_list:
+        biggest = number if biggest < number else biggest
+    return (biggest)
+
+
+if __name__ == "__main__":
+    my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+    max_value = max_integer(my_list)
+    print("Max: {}".format(max_value))
