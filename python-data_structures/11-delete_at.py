af2f4169b3509e74cb1452b9d68597799c93c509
ylabate <12196@holbertonstudents.com>
2026-01-19 14:25:21 +0000

    feat: add delete_at function to remove an element at a specified index in a list

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx > len(my_list) - 1 or idx < 0:
+        return (my_list)
+    del my_list[idx]
+    return (my_list)
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    new_list = delete_at(my_list, idx)
+    print(new_list)
+    print(my_list)
