3ff0d666279510f0569f13e31661745676e73dc6
ylabate <12196@holbertonstudents.com>
2026-01-19 09:33:14 +0000

    feat: add new_in_list function to create a copy of the list and replace an element by index

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    my_list_copy = my_list.copy()
+    if idx < len(my_list_copy) and idx >= 0:
+        my_list[idx] = element
+        return (my_list_copy)
+    else:
+        return (my_list_copy)
+
