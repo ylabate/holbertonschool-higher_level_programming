fda8baa9e581d11a67eb551ebcdd3a0354bd74b5
ylabate <12196@holbertonstudents.com>
2026-01-19 13:01:52 +0000

    fix: correct conditional check for first character in multiple_returns function

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return (len(sentence), sentence[0] if sentence[0] else None)
+    return (len(sentence), sentence[0] if sentence else None)
 
 
 if __name__ == "__main__":
     sentence = "At school, I learnt C!"
-    length, first = multiple_returns(sentence)
-    print("Length: {:d} - First character: {}".format(length, first))
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
 
