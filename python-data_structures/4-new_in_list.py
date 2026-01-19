f220759270bb6aa834aeb72cb0705f74efde3fad
ylabate <12196@holbertonstudents.com>
2026-01-19 09:33:59 +0000

    fix: correct element assignment in new_in_list function to use the copied list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     my_list_copy = my_list.copy()
     if idx < len(my_list_copy) and idx >= 0:
-        my_list[idx] = element
+        my_list_copy[idx] = element
         return (my_list_copy)
     else:
         return (my_list_copy)
