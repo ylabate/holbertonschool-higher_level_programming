6bc76badec70ee734703f86020a1b5a26241c669
ylabate <12196@holbertonstudents.com>
2026-01-20 09:11:34 +0000

    fix: remove extra spaces in set initialization for common_elements function

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -4,8 +4,8 @@ def common_elements(set_1, set_2):
 
 
 if __name__ == "__main__":
-    set_1 = {"Python", "C", "Javascript" }
-    set_2 = {"Bash", "C", "Ruby", "Perl" }
+    set_1 = {"Python", "C", "Javascript"}
+    set_2 = {"Bash", "C", "Ruby", "Perl"}
     c_set = common_elements(set_1, set_2)
     print(c_set)
     print(sorted(list(c_set)))
