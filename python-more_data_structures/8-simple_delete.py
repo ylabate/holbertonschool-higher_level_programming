88d98fd7f2e009a828ee0f6834ecad44ada2c7a2
ylabate <12196@holbertonstudents.com>
2026-01-20 09:50:47 +0000

    fix: format dictionary initialization for improved readability

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     print_sorted_dictionary = \
         __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-    a_dictionary = {'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3]}
+    a_dictionary = {'language': "C", 'Number': 89, 'track': "Low",
+                    'ids': [1, 2, 3]}
     new_dict = simple_delete(a_dictionary, 'track')
     print_sorted_dictionary(a_dictionary)
     print("--")
