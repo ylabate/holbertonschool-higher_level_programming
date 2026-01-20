a3a3deaab7e528476384254f3f9741a6a9660195
ylabate <12196@holbertonstudents.com>
2026-01-20 13:35:34 +0000

    refactor: remove debug print statement from roman_to_int function

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -15,9 +15,7 @@ def roman_to_int(roman_string):
     for letter in roman_string:
         if converter[letter]:
             number_list.append(converter[letter])
-    print(number_list)
     for i in range(len(number_list) - 1):
-        print(number_list[i])
         if number_list[i + 1] and number_list[i + 1] > number_list[i]:
             number_list[i + 1] -= number_list[i]
             number_list[i] = 0
@@ -26,8 +24,5 @@ def roman_to_int(roman_string):
 
 if __name__ == "__main__":
     roman_to_int = __import__('12-roman_to_int').roman_to_int
-
-
-
     roman_number = "XCIX"
     print("{} = {}".format(roman_number, roman_to_int(roman_number)))
