3a06f9e1740c96fe56266ecf8528d56b3845aa01
ylabate <12196@holbertonstudents.com>
2026-01-20 13:22:46 +0000

    feat: implement roman_to_int function to convert Roman numerals to integers

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    converter = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    number_list = []
+    for letter in roman_string:
+        if converter[letter]:
+            number_list.append(converter[letter])
+    for i in range(len(number_list) - 1):
+        if number_list[i + 1] and number_list[i + 1] > number_list[i]:
+            number_list[i + 1] -= number_list[i]
+            del number_list[i]
+    return (sum(number_list))
+
+
+if __name__ == "__main__":
+    roman_to_int = __import__('12-roman_to_int').roman_to_int
+
+    roman_number = "X"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "VII"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "IX"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "LXXXVII"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "DCCVII"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
