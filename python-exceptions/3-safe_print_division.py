0b4ba436f83c14b15913684fa2431d254b29fd89
ylabate <12196@holbertonstudents.com>
2026-01-21 10:54:58 +0000

    feat: fix safe_print_division to return None for division by zero

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,10 +3,10 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except (ZeroDivisionError):
-        result = 0
+        result = None
     finally:
         print("Inside result: {}".format(result))
-    return (result if result != 0 else None)
+    return (result)
 
 
 if __name__ == "__main__":
