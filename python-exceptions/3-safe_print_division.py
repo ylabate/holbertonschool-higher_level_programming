e1871c69d21d30c354b030240928db4a016c0cd1
ylabate <12196@holbertonstudents.com>
2026-01-21 10:51:48 +0000

    feat: implement safe_print_division function for safe division with error handling

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except (ZeroDivisionError):
+        result = 0
+    finally:
+        print("{}".format(result))
+        return (result if result != 0 else None)
+
+
+if __name__ == "__main__":
+    a = 12
+    b = 2
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
+
+    a = 12
+    b = 0
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
