95bfe9c1441b2d9ab872d49cad2c244c2561f711
ylabate <12196@holbertonstudents.com>
2026-01-21 13:38:43 +0000

    feat: add list_division function for safe list operations

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            result.append(my_list_1[i] / my_list_2[i])
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+        finally:
+            try:
+                if result[i]:
+                    pass
+            except IndexError:
+                result.append(0)
+    return result
+
+
+if __name__ == "__main__":
+    my_l_1 = [10, 8, 4]
+    my_l_2 = [2, 4, 4]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)
+
+    print("--")
+
+    my_l_1 = [10, 8, 4, 4]
+    my_l_2 = [2, 0, "H", 2, 7]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)
