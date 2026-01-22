5b96ebafe4656b13099d7fec323a8e8b425b6c08
ylabate <12196@holbertonstudents.com>
2026-01-22 14:16:22 +0000

    feat: add print_square function with input validation and comprehensive test cases

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+That a module that contains print_square a function that print a square
+of # in specifique size
+"""
+
+
+def print_square(size):
+    """Print a square of a specifique size
+
+    This function prints a square of # in specifique size
+    to the standard output.
+
+    Args:
+        size (int): The size of the Square to be printed
+
+    Returns:
+        int: the number of # printed
+
+    Raises:
+        TypeError: If size is not a integer
+
+    Example:
+        >>> print_square(4)
+        ####
+        ####
+        ####
+        ####
+    """
+    if not isinstance(size, int) or isinstance(size, bool):
+        raise TypeError("size must be an integer")
+    elif size <= 0:
+        raise ValueError("size must be >= 0")
+    print('\n'.join(['#' * size for i in range(size)]))
