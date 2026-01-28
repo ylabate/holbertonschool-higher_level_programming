a15d0f15758116302276156335f5866bc3ce8299
ylabate <12196@holbertonstudents.com>
2026-01-28 11:04:42 +0000

    feat: implement Rectangle class with properties, methods, and instance management

---
+++ python-more_classes/7-rectangle.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python3
+"""Module defining a Rectangle class.
+
+This module contains the definition of the Rectangle class,
+which defines a rectangle by its width and height.
+"""
+
+
+class Rectangle:
+    """Represents a rectangle.
+
+    Attributes:
+        number_of_instances (int): The number of Rectangle instances.
+        print_symbol (any): The symbol used for string representation.
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+    """
+    print_symbol = "#"
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """Initializes a new Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle. Defaults to 0.
+            height (int): The height of the rectangle. Defaults to 0.
+        """
+        Rectangle.number_of_instances += 1
+        self.width = width
+        self.height = height
+
+    def __del__(self):
+        """Prints a message when an instance of Rectangle is deleted."""
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
+
+    @property
+    def width(self):
+        """int: Retrieves the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width of the rectangle.
+
+        Args:
+            value (int): The new width of the rectangle.
+
+        Raises:
+            TypeError: If width is not an integer.
+            ValueError: If width is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """int: Retrieves the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height of the rectangle.
+
+        Args:
+            value (int): The new height of the rectangle.
+
+        Raises:
+            TypeError: If height is not an integer.
+            ValueError: If height is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns the rectangle area.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Returns the rectangle perimeter.
+
+        Returns:
+            int: The perimeter of the rectangle, or 0 if width or height is 0.
+        """
+        return (self.__width + self.__height
+                ) * 2 if self.__width and self.__height else 0
+
+    def __str__(self):
+        """Returns the printable string representation of the rectangle.
+
+        The rectangle is represented by the print_symbol attribute.
+
+        Returns:
+            str: The formatted string representing the rectangle.
+        """
+        res = ""
+        if self.__width > 0 and self.__height:
+            line = str(self.print_symbol) * self.__width
+            res += "\n".join(line for _ in range(self.__height))
+        return res
+
+    def __repr__(self):
+        """Returns a string representation of the rectangle to recreate it."""
+        return f"Rectangle({self.__width}, {self.__height})"
+
+
+if __name__ == "__main__":
+    my_rectangle_1 = Rectangle(8, 4)
+    print(my_rectangle_1)
+    print("--")
+    my_rectangle_1.print_symbol = "&"
+    print(my_rectangle_1)
+    print("--")
+
+    my_rectangle_2 = Rectangle(2, 1)
+    print(my_rectangle_2)
+    print("--")
+    Rectangle.print_symbol = "C"
+    print(my_rectangle_2)
+    print("--")
+
+    my_rectangle_3 = Rectangle(7, 3)
+    print(my_rectangle_3)
+
+    print("--")
+
+    my_rectangle_3.print_symbol = ["C", "is", "fun!"]
+    print(my_rectangle_3)
+
+    print("--")
