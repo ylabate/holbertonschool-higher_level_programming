5d9a81213136daf85d1b2c77166fab5c9a2d9e91
ylabate <12196@holbertonstudents.com>
2026-02-02 15:18:58 +0000

    feat: add SwimMixin and FlyMixin classes with static methods for swimming and flying behaviors

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+class SwimMixin:
+    @staticmethod
+    def swim():
+        print("The creature swims!")
+
+
+class FlyMixin:
+    @staticmethod
+    def fly():
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    @staticmethod
+    def roar():
+        print("The dragon roars")
