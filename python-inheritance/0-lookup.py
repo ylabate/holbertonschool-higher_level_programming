2c7e825479398c1f30220b618735715b07ea85b3
ylabate <12196@holbertonstudents.com>
2026-02-02 09:31:41 +0000

    feat: add lookup function and module documentation for object attribute inspection

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+"""
+Module for looking up attributes and methods of objects.
+Provides a function to return a list of all attributes and methods of any object.
+"""
+
+
+def lookup(obj):
+    """
+    Return a list of available attributes and methods of an object.
+
+    :param obj: Any object to inspect
+    :return: List of strings containing the names of all attributes and methods
+    """
+    return dir(obj)
