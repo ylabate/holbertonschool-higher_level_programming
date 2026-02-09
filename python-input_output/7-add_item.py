7585ab7a87f4c26206aa503c3aa55744eb2c792e
ylabate <12196@holbertonstudents.com>
2026-02-09 10:30:09 +0000

    feat: modify add_item.py to load existing items and append new ones from command line arguments

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,5 +4,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def main():
-    save_to_json_file(argv[1], "add_item.json")
+if __name__ == "__main__":
+    list = load_from_json_file("add_item.json")
+    list += argv[1:]
+    save_to_json_file(list, "add_item.json")
