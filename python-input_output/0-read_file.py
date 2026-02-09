a8ccf9d9fd363f44dc2a46df7398424a93e69c2f
ylabate <12196@holbertonstudents.com>
2026-02-09 10:17:30 +0000

    feat: add shebang to Python files and implement save_to_json_file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def read_file(filename=""):
     with open(filename, "r") as file:
         print(file.read())
--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def write_file(filename="", text=""):
     with open(filename, "w") as file:
         file.write(text)
