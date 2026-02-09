b1bb913645e48a41b5cfac77be0c37671b46aba0
ylabate <12196@holbertonstudents.com>
2026-02-09 10:27:28 +0000

    chore: change file permissions to make Python scripts executable

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def main():
+    save_to_json_file(argv[1], "add_item.json")
