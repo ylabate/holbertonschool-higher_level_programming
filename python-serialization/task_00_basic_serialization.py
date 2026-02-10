ca4a30c9d50b3a2206eb01ab848a12ed5d1c6efc
ylabate <12196@holbertonstudents.com>
2026-02-10 15:51:36 +0100

    feat: add basic serialization functions for JSON data handling

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    with open(filename, "w") as file:
+        json.dump(data, file)
+
+
+def load_and_deserialize(filename):
+    with open(filename, "r") as file:
+        return json.load(file)
