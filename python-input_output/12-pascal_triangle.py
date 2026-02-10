0a958d517b2ae9a9ae51008297360d397301c163
ylabate <12196@holbertonstudents.com>
2026-02-10 15:01:28 +0100

    refactor: optimize pascal_triangle function for clarity and performance

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -2,12 +2,10 @@ def pascal_triangle(n):
     triangle = []
     for layer in range(1, n + 1):
         row = []
-        row.append(1)
-        for i in range(layer - 1, 0, -1):
-            row.append(i)
+        for i in range(layer):
+            if i == layer - 1 or i == 0:
+                row.append(1)
+            else:
+                row.append(triangle[layer - 2][i - 1] + triangle[layer - 2][i])
         triangle.append(row)
     return triangle
-
-
-for line in pascal_triangle(5):
-    print(line)
