69db8f6a79ddff280e3c642c70e4573503fe49e6
ylabate <12196@holbertonstudents.com>
2026-02-11 09:18:41 +0000

    Chore: Remove unused import and set exec bit

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """Module that returns the dictionary description with simple structure."""
 
-import json
-
 
 def class_to_json(obj):
     """Returns the dictionary description with simple data structure.
