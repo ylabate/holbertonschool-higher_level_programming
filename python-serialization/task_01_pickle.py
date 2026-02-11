d54a5616344a043b54236586fd28d364e6cbd7b8
ylabate <12196@holbertonstudents.com>
2026-02-11 09:44:52 +0000

    Chore: remove demo code from pickle example

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -21,17 +21,3 @@ class CustomObject:
     def deserialize(cls, filename):
         with open(filename, "rb") as file:
             return pickle.load(file)
-
-
-# Create an instance of CustomObject
-obj = CustomObject(name="John", age=25, is_student=True)
-print("Original Object:")
-obj.display()
-
-# Serialize the object
-obj.serialize("object.pkl")
-
-# Deserialize the object into a new instance
-new_obj = CustomObject.deserialize("object.pkl")
-print("\nDeserialized Object:")
-new_obj.display()
