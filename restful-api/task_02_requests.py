07afdd367df4eaf92e6069217b761b657b53fcf0
ylabate <12196@holbertonstudents.com>
2026-02-16 16:47:46 +0100

    feat(restful-api): Add CSV export for posts
    Add functions to fetch posts from JSONPlaceholder and save them to
    posts.csv using csv.DictWriter. Also provide fetch_and_print_posts to
    print post titles and invoke fetch_and_save_posts at module end.

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 import requests
+import csv
 
-if __name__ == "__main__":
-    print(requests.get("https://jsonplaceholder.typicode.com/post/1"))
+def fetch_and_print_posts():
+    page = requests.get("https://jsonplaceholder.typicode.com/posts/")
+    print(f"Status Code: {page.status_code}")
+    print('\n'.join(post['title'] for post in page.json()))
+
+def fetch_and_save_posts():
+    res = requests.get("https://jsonplaceholder.typicode.com/posts/").json()
+    data = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in res]
+    with open("posts.csv", "w", newline='') as file:
+        writer = csv.DictWriter(file, ['id', 'title',  'body'])
+        writer.writeheader()
+        writer.writerows(data)
