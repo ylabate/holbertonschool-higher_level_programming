4addb4374148126d69577b4def6f0f47239298b4
ylabate <12196@holbertonstudents.com>
2026-02-18 09:01:32 +0000

    Feat: add /status and remove newline from 404

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -16,11 +16,16 @@ class Server(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(data).encode('utf-8'))
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"OK")
         else:
             self.send_response(404)
             self.send_header('content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"404 Not Found\n")
+            self.wfile.write(b"404 Not Found")
 
 
 server = http.server.HTTPServer(('localhost', 8000), Server)
