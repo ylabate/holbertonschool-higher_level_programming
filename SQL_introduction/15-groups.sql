c80a3818d04b26f3720929163cb5708c31cd929e
ylabate <12196@holbertonstudents.com>
2026-03-04 10:41:52 +0000

    feat: Sort grouped scores in descending order

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- Lists the number of records with the same score in the table second_table
 -- Display: score and number of records for this score
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
