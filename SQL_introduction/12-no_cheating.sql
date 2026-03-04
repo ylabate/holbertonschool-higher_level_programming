8d706299185168b1bdccc893c3888a8c26323e02
ylabate <12196@holbertonstudents.com>
2026-03-04 10:39:18 +0000

    Fix(sql): Update only Bob's score

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,3 @@
 -- Updates the score of Bob to 10 in the second_table
 -- Update score where score >= 10
-UPDATE second_table SET score = 10 WHERE score >= 10;
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
