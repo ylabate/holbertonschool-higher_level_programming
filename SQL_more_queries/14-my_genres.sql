1e92888f0b66dc281b1646092c1efac26e6059b4
ylabate <12196@holbertonstudents.com>
2026-03-04 14:29:26 +0000

    Update 14-my_genres.sql

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -4,4 +4,4 @@ FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_shows.title ASC
+ORDER BY tv_genres.name ASC
