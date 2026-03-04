9937a6ab93d6506968e3a630f5c97f933cede3dc
ylabate <12196@holbertonstudents.com>
2026-03-04 14:26:34 +0000

    feat: Add SQL query to list genres for Dexter

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- Lists all genres of the show Dexter from hbtn_0d_tvshows
+SELECT tv_genres.name
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = 'Dexter'
