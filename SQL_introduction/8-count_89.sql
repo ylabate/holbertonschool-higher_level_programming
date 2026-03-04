a3b897d128a338fbc622fe45abb186eb78ffea9d
ylabate <12196@holbertonstudents.com>
2026-03-04 10:37:26 +0000

    Fix(sql): Count rows where id = 89

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- Displays the number of records with id = 89 in the table first_table
 -- Count the number of records with id value = 89
-SELECT COUNT(id) AS records FROM first_table;
+SELECT COUNT(id) AS records FROM first_table WHERE id = 89;
