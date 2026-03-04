75822bb78c45a538fe51a2000fe8d91f22bf93f2
ylabate <12196@holbertonstudents.com>
2026-03-04 14:41:45 +0000

    Feat(tvshows): Add query to list Comedy shows

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- Lists all Comedy shows from hbtn_0d_tvshows
+SELECT tv_shows.title
+FROM tv_show_genres
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC
