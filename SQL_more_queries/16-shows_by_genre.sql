4ec0054bf39f95f8ac6948e849b7557fd74e208a
ylabate <12196@holbertonstudents.com>
2026-03-04 14:51:01 +0000

    Feat(sql): Add query listing shows and genres

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Lists all shows and all genres linked to each show from hbtn_0d_tvshows
+SELECT tv_shows.title, tv_genres.name
+FROM tv_show_genres
+RIGHT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_genres.name ASC
