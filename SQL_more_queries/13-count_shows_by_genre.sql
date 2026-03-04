db3d658110e594452950437a35027e78034d4df8
ylabate <12196@holbertonstudents.com>
2026-03-04 14:10:32 +0000

    Refactor(sql): Alias tv_genres.name as genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Lists all genres and the number of shows linked to each one
-SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
