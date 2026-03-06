47679ee5817a29cdbdf331d696559442732669fd
ylabate <12196@holbertonstudents.com>
2026-03-06 16:54:30 +0000

    Add docstrings to 0-select_states script

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa."""
+
 from sys import argv
 
 import MySQLdb
 
 
 def main():
+    """Connect to MySQL and list all states sorted by id ascending."""
     with MySQLdb.connect(
         host="localhost",
         port=3306,
