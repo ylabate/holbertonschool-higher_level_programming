a068a26e1c481a986ab6a733bb3095d50422a3b1
ylabate <12196@holbertonstudents.com>
2026-03-06 17:02:06 +0000

    Add script to filter states by name

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Script that lists all states matching a given name
+    from the database hbtn_0e_0_usa."""
+
+from sys import argv
+
+import MySQLdb
+
+
+def main():
+    """Connect to MySQL and list all states whose name matches the user input,
+        sorted by id ascending."""
+    with MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+    ) as connexion:
+        with connexion.cursor() as cur:
+            cur.execute("SELECT * FROM states ORDER BY id ASC")
+            for row in cur.fetchall():
+                if row[1] == argv[4]:
+                    print(row)
+
+
+if __name__ == "__main__":
+    main()
