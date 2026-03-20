4fdbb07777bf5af76f623e6eebfddd2014c1ae76
ylabate <12196@holbertonstudents.com>
2026-03-20 15:55:26 +0100

    Fix: Print second largest number

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -3,4 +3,4 @@ const args = process.argv.slice(2).map(Number);
 
 if (args.length <= 1) { console.log(0); }
 
-console.log(args.sort((a, b) => b - a)[0]);
+console.log(args.sort((a, b) => b - a)[1]);
