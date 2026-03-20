2504938893c5696e22329986a851ed82790ff20e
ylabate <12196@holbertonstudents.com>
2026-03-20 15:22:17 +0100

    feat: Add script to print square of given size

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const x = process.argv[2];
+if (isNaN(x)) {
+  console.log('Missing size');
+}
+
+let row = ''
+for (let j = 0; j < x; j++) {
+  row += '#'
+}
+
+for (let i = 0; i < x; i++) {
+  console.log(row);
+}
