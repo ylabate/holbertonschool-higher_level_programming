770d7dbd3083a533f2638bc0052e674a99dc9823
ylabate <12196@holbertonstudents.com>
2026-03-20 15:57:47 +0100

    style: Format conditional block for readability

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 const args = process.argv.slice(2).map(Number);
 
-if (args.length <= 1) { console.log(0); }
-
-console.log(args.sort((a, b) => b - a)[1]);
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  console.log(args.sort((a, b) => b - a)[1]);
+}
