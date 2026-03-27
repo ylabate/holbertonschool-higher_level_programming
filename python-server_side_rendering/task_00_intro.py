d785ff5980f1b67e7f259d788bb2eb0d642d2241
ylabate <12196@holbertonstudents.com>
2026-03-27 19:12:29 +0100

    Update task_00_intro.py

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -3,19 +3,19 @@ import os
 
 
 def generate_invitations(template, attendees):
-    errTxt = f", no output files generated."
-    if not template:
-        print(f"Template is empty{errTxt}", file=sys.stderr, end='')
-    elif not attendees:
-        print(f"No data provided{errTxt}", file=sys.stderr, end='')
+    errTxt = ", no output files generated."
+    if not template or template == "":
+        print(f"Template is empty{errTxt}", file=sys.stderr)
+    elif not attendees or attendees == []:
+        print(f"No data provided{errTxt}", file=sys.stderr)
     elif not isinstance(template, str):
-        print(f"template is {type(template)}, string expected{errTxt}", file=sys.stderr, end='')
+        print(f"template is {type(template)}, string expected{errTxt}", file=sys.stderr)
     elif not isinstance(attendees, list):
-        print(f"template is {type(attendees)}, a list of dictionnary expected{errTxt}", file=sys.stderr, end='')
+        print(f"template is {type(attendees)}, a list of dictionnary expected{errTxt}", file=sys.stderr)
     else:
         for i, attendee in enumerate(attendees, 1):
             if not isinstance(attendee, dict):
-                print(f"template is {type(attendee)}, a list of dictionnary expected{errTxt}", file=sys.stderr, end='')
+                print(f"template is {type(attendee)}, a list of dictionnary expected{errTxt}", file=sys.stderr)
                 return
             buffer = template
             for key, value in attendee.items():
